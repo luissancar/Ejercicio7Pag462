ccaf007b8a18bfb287d8618799d034fec4608a66
luissancar <luissancar@yahoo.es>
2016-08-26 18:52:47 +0200

    Segunda

--- app/src/main/java/com/example/luissancar/ejercicio7pag46/MainActivity.java
+++ app/src/main/java/com/example/luissancar/ejercicio7pag46/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.luissancar.ejercicio7pag46;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -17,6 +20,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         tv1=(TextView)findViewById(R.id.textView);
         lv1=(ListView)findViewById(R.id.listView);
-        
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, ciudades);
+        lv1.setAdapter(adapter);
+        lv1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView parent, View view, int position, long id) {
+                tv1.setText("Poblacion de " + lv1.getItemAtPosition(position) + " es " + habitantes[position]);
+            }
+        });
+
+
     }
 }
